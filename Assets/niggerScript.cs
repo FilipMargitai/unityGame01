cc64a0f63c23c809ed235ebc5411324f6f871f86
FilipMargitai <filip.margitai@pslib.cz>
2020-10-13 11:12:36 +0200

    project import commit

---
+++ Assets/niggerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class niggerScript : MonoBehaviour
+{
+    private int i;
+    public int c = 2;
+    public GameObject Gay;
+    public GameObject gay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Gay.GetComponent<BoxCollider>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Debug.Log("nigger" + i);
+        i++;
+
+        gay.GetComponent<Rigidbody>().AddForce(new Vector3(10,0,0));
+    }
+}
